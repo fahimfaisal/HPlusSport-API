fa7a463920d14c6bfde2ef1c80f4742f73088cf9
fahimfaisal <navid2010ps@gmail.com>
2021-02-10 22:14:52 +1100

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts() {
+            return "OK.";
+        }
+    }
+}
---
+++ HPlusSport.API/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+
+        public string MyProperty { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
